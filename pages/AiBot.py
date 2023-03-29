df6a070a37811d28dba6c3f8b2cc14efeaae7ad3
Sohaib <sohaibmaqsood63@gmail.com>
2023-03-30 00:34:29 +0500

    change the old api to better one

--- pages/AiBot.py
+++ pages/AiBot.py
@@ -1,22 +1,21 @@
 import requests
 import streamlit as st
 
-url = "https://you-chat-gpt.p.rapidapi.com/"
+url = "https://scodex-api.p.rapidapi.com/"
 
 st.title("Ai in your Service")
 query = st.text_area("Ask me anything👇")
 response_btn = st.button("Get Response")
+
 if response_btn:
-    payload = {
-        "question": query,
-        "max_response_time": 20
-    }
+    payload = {"prompt": query}
     headers = {
         "content-type": "application/json",
+        "Content-Type": "application/json",
         "X-RapidAPI-Key": "92e0b3f2b4msh3c165a5b3bb5840p12dc96jsn4368636b3287",
-        "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com"
+        "X-RapidAPI-Host": "scodex-api.p.rapidapi.com"
     }
 
     response = requests.request("POST", url, json=payload, headers=headers)
     answer = response.json()
-    st.info(answer["answer"])
+    st.info(answer["bot"])
